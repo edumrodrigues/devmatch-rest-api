aac833fb1efd21804bc01480cbfe01f5af44c5e8
Eduardo Rodrigues <mrodrigues.eduardo@gmail.com>
2017-01-09 22:27:55 -0200

    Create base rest project

---
+++ src/main/java/com/devmatch/DevmatchApplication.java
@@ -0,0 +1,12 @@
+package com.devmatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevmatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevmatchApplication.class, args);
+	}
+}
---
+++ src/test/java/com/devmatch/DevmatchApplicationTests.java
@@ -0,0 +1,16 @@
+package com.devmatch;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DevmatchApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
