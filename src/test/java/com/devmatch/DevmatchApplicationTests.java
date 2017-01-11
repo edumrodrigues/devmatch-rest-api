4185508dfd1e68d1e32aed650ba2f43a72223e7b
Eduardo Rodrigues <mrodrigues.eduardo@gmail.com>
2017-01-11 00:19:28 -0200

    Create first test

--- src/test/java/com/devmatch/DevmatchApplicationTests.java
+++ src/test/java/com/devmatch/DevmatchApplicationTests.java
@@ -1,16 +1,30 @@
 package com.devmatch;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import com.devmatch.model.Person;
+import com.devmatch.repository.PersonRepository;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class DevmatchApplicationTests {
+	
+	@Autowired
+	private PersonRepository personRepository;
 
 	@Test
 	public void contextLoads() {
+		Person person = new Person();
+		person.setFirstName("Eduardo");
+		person.setLastName("Rodrigues");
+		personRepository.save(person);
+		assertThat(personRepository.count()).isEqualTo(1);
 	}
 
 }
